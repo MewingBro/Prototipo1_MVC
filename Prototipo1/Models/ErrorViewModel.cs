cde2939cde4ec801d031915ad0297f1d49795835
Johery Zamora <johereklio34zb@gmail.com>
2025-08-07 00:34:41 -0600

    Agregar archivos de proyecto.

---
+++ Prototipo1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prototipo1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
